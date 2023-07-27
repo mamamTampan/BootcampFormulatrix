691797406f98bf312de87643759501de7c18ba79
Irfan Maulana <irfan.maoelana@gmail.com>
2023-07-27 23:02:37 +0700

    update

--- MyProject/MonopolyProject/Source/Tile.cs
+++ MyProject/MonopolyProject/Source/Tile.cs
@@ -2,5 +2,26 @@ namespace MonopolyProject.Source;
 
 public abstract Tile
 {
+	protected TileType _type
+	protected string _name;
+	protected int _location;
+	protected string _description;
+	
+	public GetType()
+	{
+		TileType
+	}
+	public  GetName()
+	{
+		string
+	}
+	public GetLocation()
+	{
+		int
+	}
+	public GetDescription()
+	{
+		string
+	}	
 	
 }
