47a41051be456a649b71befbad12bb531cf02324
Irfan Maulana <irfan.maoelana@gmail.com>
2023-08-02 18:01:35 +0700

    update

---
+++ Week 4/Day 1/Program.cs
@@ -0,0 +1,44 @@
+﻿#define Release
+
+using System;
+
+namespace MonopolyGame
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Welcome to Monopoli Game !\n");
+			#if Debug
+				RollDice();	
+				DrawChanceCard();
+				SellProperty("Canada");
+			#elif Release
+				int diceValue = RollDice();
+				Console.WriteLine($"Throw dice: {diceValue}\n");
+				DrawChanceCard();
+				SellProperty("Canada");
+				Console.WriteLine("U're Bankrupt\n");
+			#endif
+			Console.WriteLine("Thanks for play Monopoli Game.");
+			Console.ReadLine();
+		}
+
+		static int RollDice()
+		{
+			Random random = new Random();
+			return random.Next(2, 12);
+		}
+
+		static void SellProperty(string propertyName)
+		{
+			Console.WriteLine($"Player Sell property: {propertyName}\n");
+		}
+
+		static void DrawChanceCard()
+		{
+			Console.WriteLine("Player take a Chance Card");
+			Console.WriteLine("U go to in Jail\n");
+		}
+	}
+}
---
+++ Week 4/Day 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncAsyncExample
+{
+	class Program
+	{
+		static async Task Main()
+		{
+			DoSyncTask();
+			await AsyncTask();
+		}
+
+		static void DoSyncTask()
+		{
+			Console.WriteLine("Thread 1 Started.");
+			Thread.Sleep(100);
+			Console.WriteLine("Thread 2 Started.");
+			Thread.Sleep(1000);
+			Console.WriteLine("All Thread Done.");
+		}
+		static async Task AsyncTask()
+		{
+			Console.WriteLine("Task 1 Started.");
+			await Task.Delay(2000);
+			Console.WriteLine("Task 2 Started.");
+			await Task.Delay(200);
+			Console.WriteLine("All Task Done.");
+		}
+	}
+}
