dec0df786b74e7e090d60e6f28dee659a97b16c9
Irfan <irfan.maoelana@gmail.com>
2023-08-02 11:38:57 +0700

    uodate

---
+++ Week 4/Day 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+public class Program
+{
+	static Semaphore semaphore = new Semaphore(5, 5);
+
+	static async Task Main()
+	{
+		Task task = Task.Run(() => DoWork());
+		await task;
+		Console.ReadLine();
+	}
+
+	static async Task DoWork( )
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+		Console.WriteLine($"Task {i} mulai");
+		semaphore.WaitOne();
+		
+		Console.WriteLine($"Task {i} berjalan");
+		await Task.Delay(123);
+		}
+		Console.WriteLine($"Task selesai");
+		semaphore.Release();
+	}
+}
