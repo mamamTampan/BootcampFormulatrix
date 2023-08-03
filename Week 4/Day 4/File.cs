fe43c8a53788436cfd14f46533a2753252b57804
Irfan Maulana <irfan.maoelana@gmail.com>
2023-08-03 17:12:59 +0700

    up

---
+++ Week 4/Day 4/File.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+
+namespace ListItem
+{
+	public class File
+	{
+		private string path;
+		private FileInfo info;
+		private StreamWriter str;
+		private string input = "";
+
+		public File()
+		{
+			path = @"List Item.txt";
+			info = new FileInfo(path);
+			if (info.Exists)
+			{
+				str = info.AppendText();
+			}
+			else
+			{
+				str = info.CreateText();
+			}
+		}
+
+		public void CreateFile()
+		{
+			str.Close();
+			info.Create();
+			Console.WriteLine("File has been created");
+			Console.ReadKey();
+		}
+
+		public void CreateTextAuto()
+		{
+			str.WriteLine("My List Item");
+			str.WriteLine("1. PC");
+			str.WriteLine("2. Monitor");
+			str.WriteLine("3. Mouse");
+			str.WriteLine("4. Keyboard");
+			str.Close();
+			Console.WriteLine("File has been created with text");
+			Console.ReadKey();
+		}
+
+		public void CreateTextManual()
+		{
+			Console.Write("Enter text: ");
+			input = Console.ReadLine();
+			str.Write(input);
+			str.Close();
+			Console.WriteLine("File has been created with text");
+			Console.ReadKey();
+		}
+
+		public void DeleteFile()
+		{
+   			if (info.Exists)
+			{
+	   		str.Close();
+			info.Delete();
+			Console.WriteLine("File has been deleted");
+			}
+			else
+			{
+			Console.WriteLine("File doesn't exist");
+			}
+   	 		Console.ReadKey();
+		}
+
+	}
+}
--- Week 4/Day 4/Program.cs
+++ Week 4/Day 4/Program.cs
@@ -1,51 +1,37 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace ListItem
 {
-	class Program
-	{
-		static void Main()
-		{
-			string path = @"ListItem.txt";
-			FileInfo file = new(path);
-			StreamWriter str = new(path);
+    class Program
+    {
+        static void Main()
+        {
+            File file = new File();
+            
+            Console.WriteLine("Choose Menu");
+            Console.WriteLine("1. Create File");
+            Console.WriteLine("2. Create Text in File");
+            Console.WriteLine("3. Create Custom Text in File");
+            Console.WriteLine("4. Delete File");
+            Console.Write("Choose : ");
 
-			Console.WriteLine("Choose Menu");
-			Console.WriteLine("1.Create File");
-			Console.WriteLine("2.Create Text in File");
-			Console.WriteLine("2.Create Custom Text in File");
-			Console.WriteLine("4.Delete FIle");
-			Console.Write("Choose : ");
-
-			int n = Convert.ToInt32(Console.ReadLine());
-			switch (n)
-			{
-				case 1:
-					file.Create();
-					Console.WriteLine("File has been created");
-					Console.ReadKey();
-					break;
-				case 2:
-					str.WriteLine("My List Item");
-					str.WriteLine("1. PC");
-					str.WriteLine("2. Monitor");
-					str.WriteLine("3. Mouse");
-					str.WriteLine("4. Keyboard");
-					Console.WriteLine("File has been created with text");
-					str.Close();
-					break;
-				case 3:
-					string input = Console.ReadLine();
-					str.Write(input);
-					Console.WriteLine("File has been created with text");
-					break;
-				case 4:
-					file.Delete();
-					Console.WriteLine("File has been deleted");
-					Console.ReadKey();
-					break;
-			}
-
-		}
-	}
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    file.CreateFile();
+                    break;
+                case 2:
+                    file.CreateTextAuto();
+                    break;
+                case 3:
+                    file.CreateTextManual();
+                    break;
+                case 4:
+                    file.DeleteFile();
+                    break;
+            }
+        }
+    }
 }
