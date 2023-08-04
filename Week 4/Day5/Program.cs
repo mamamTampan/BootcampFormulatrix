59a50507316b7fc00b3821df877a84745a43a188
Irfan Maulana <irfan.maoelana@gmail.com>
2023-08-04 18:40:59 +0700

    up

--- Week 4/Day5/Program.cs
+++ Week 4/Day5/Program.cs
@@ -1,8 +1,7 @@
-﻿using System.Runtime.Serialization.DataContracts;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization;
-using System.Xml.Serialization;
-using System;
-
 
 namespace MonopolyProject
 {
@@ -10,73 +9,85 @@ namespace MonopolyProject
 	public class Tile
 	{
 		[DataMember]
-		private int loc {get;set;}
+		internal int Location { get; private set; }
+
 		[DataMember]
-		private string name{get;set;}
+		public string Names { get; private set; }
+
 		[DataMember]
-		private string desc{get;set;}
-	}
-	
+		public string Description { get; private set; }
 		
-	
+		public Tile(int location, string names, string description)
+		{
+			Location = location;
+			Names = names;
+			Description = description;
+		}
+	}
+
 	class Program
 	{
 		static void Main()
 		{
-			private List<Tile> tile = new List<Tile>
+			List<Tile> tiles = new List<Tile>
 			{
-			new Tile {loc = 1, name = "Start", desc = "$50"},
-			new Tile {loc = 2, name = "Malaysia", desc = "$60"},
-			new Tile {loc = 3, name = "Community Chest", desc = "Take a Community Card"},
-			new Tile {loc = 4, name = "Singapore", desc = "$100"},
-			new Tile {loc = 5, name = "Income Tax", desc = "Pay $200"},
-			new Tile {loc = 6, name = "Terminal", desc = "$200"},
-			new Tile {loc = 7, name = "Taiwan", desc = "$100"},
-			new Tile {loc = 8, name = "Chance", desc = "Take a Chance Card"},
-			new Tile {loc = 9, name = "Hongkong", desc = "$100"},
-			new Tile {loc = 10, name = "China", desc = "$120"},
-			new Tile {loc = 11, name = "Jail", desc = "Just Visited"},
-			new Tile {loc = 12, name = "Vietnam", desc = "$140"},
-			new Tile {loc = 13, name = "Electric Company", desc = "$150"},
-			new Tile {loc = 14, name = "Camboja", desc = "$140"},
-			new Tile {loc = 15, name = "Thailand", desc = "$160"},
-			new Tile {loc = 16, name = "Station", desc = "$200"},
-			new Tile {loc = 17, name = "England", desc = "$180"},
-			new Tile {loc = 18, name = "Community Chest", desc = "Take a Community Card"},
-			new Tile {loc = 19, name = "Finland", desc = "$180"},
-			new Tile {loc = 20, name = "Scotland", desc = "$200"},
-			new Tile {loc = 21, name = "Free Parking", desc = "Enjoy Here"},
-			new Tile {loc = 22, name = "France", desc = "$220"},
-			new Tile {loc = 23, name = "Chance", desc = "$Take a Chance Card"},
-			new Tile {loc = 24, name = "Italy", desc = "$220"},
-			new Tile {loc = 25, name = "Netherland", desc = "$240"},
-			new Tile {loc = 26, name = "Port", desc = "$200"},
-			new Tile {loc = 27, name = "Australia", desc = "$260"},
-			new Tile {loc = 28, name = "New Zealand", desc = "$260"},
-			new Tile {loc = 29, name = "Water Works", desc = "$150"},
-			new Tile {loc = 30, name = "Swiss", desc = "$280"},
-			new Tile {loc = 31, name = "Go to Jail", desc = "Stay in Jail Now"},
-			new Tile {loc = 32, name = "Brazil", desc = "$300"},
-			new Tile {loc = 33, name = "Argentina", desc = "$300"},
-			new Tile {loc = 34, name = "Community Chest", desc = "Take a Community Card"},
-			new Tile {loc = 35, name = "USA", desc = "$320"},
-			new Tile {loc = 36, name = "Airport", desc = "$200"},
-			new Tile {loc = 37, name = "Chance", desc = "$Take a Chance Card"},
-			new Tile {loc = 38, name = "Africa", desc = "$350"},
-			new Tile {loc = 39, name = "Luxury Tax", desc = "Pay $100"},
-			new Tile {loc = 40, name = "Indonesia", desc = "$400"}
+				new Tile (1,"Start","$50"),
+				new Tile (2,"Malaysia","$60"),
+				new Tile (3,"Community Chest","Take a Community Card"),
+				new Tile (4,"Singapore","$100"),
+				new Tile (5,"Income Tax","Pay $200"),
+				new Tile (6,"Terminal","$200"),
+				new Tile (7,"Taiwan","$100"),
+				new Tile (8,"Chance","Take a Chance Card"),
+				new Tile (9,"Hongkong","$100"),
+				new Tile (10,"China","$120"),
+				new Tile (11,"Jail","Just Visited"),
+				new Tile (12,"Vietnam","$140"),
+				new Tile (13,"Electric Company","$150"),
+				new Tile (14,"Camboja","$140"),
+				new Tile (15,"Thailand","$160"),
+				new Tile (16,"Station","$200"),
+				new Tile (17,"England","$180"),
+				new Tile (18,"Community Chest","Take a Community Card"),
+				new Tile (19,"Finland","$180"),
+				new Tile (20,"Scotland","$200"),
+				new Tile (21,"Free Parking","Enjoy Here"),
+				new Tile (22,"France","$220"),
+				new Tile (23,"Chance","$Take a Chance Card"),
+				new Tile (24,"Italy","$220"),
+				new Tile (25,"Netherland","$240"),
+				new Tile (26,"Port","$200"),
+				new Tile (27,"Australia","$260"),
+				new Tile (28,"New Zealand","$260"),
+				new Tile (29,"Water Works","$150"),
+				new Tile (30,"Swiss","$280"),
+				new Tile (31,"Go to Jail","Stay in Jail Now"),
+				new Tile (32,"Brazil","$300"),
+				new Tile (33,"Argentina","$300"),
+				new Tile (34,"Community Chest","Take a Community Card"),
+				new Tile (35,"USA","$320"),
+				new Tile (36,"Airport","$200"),
+				new Tile (37,"Chance","$Take a Chance Card"),
+				new Tile (38,"Africa","$350"),
+				new Tile (39,"Luxury Tax","Pay $100"),
+				new Tile (40,"Indonesia","$400")
 			};
-		
-			var ser = new DataContractSerializer(typeof(List<Tile>));		
-			using (FileStream stream = new FileStream("Tile.xml",FileMode.OpenOrCreate))
+
+			using (FileStream stream = new FileStream("Tile.xml", FileMode.Create))
 			{
-				
+				DataContractSerializer serializer = new DataContractSerializer(typeof(List<Tile>));
+				serializer.WriteObject(stream, tiles);
 			}
-			List<Tile> import = (List<Tile>)serializer.Deserialize(reader);
-			foreach (var Tile in import)
-				{
-					Console.WriteLine($"Tile : {Tile.loc}\t| {Tile.name}, {Tile.desc}");
-				}
+			List<Tile> importedTiles;
+			using (FileStream stream = new FileStream("Tile.xml", FileMode.Open))
+			{
+				DataContractSerializer serializer = new DataContractSerializer(typeof(List<Tile>));
+				importedTiles = (List<Tile>)serializer.ReadObject(stream);
+			}
+			Console.WriteLine("Tiles:");
+			foreach (var tile in importedTiles)
+			{
+				Console.WriteLine($"Tile: {tile.Location},\nName: {tile.Names},    Description: {tile.Description}");
 			}
 		}
 	}
